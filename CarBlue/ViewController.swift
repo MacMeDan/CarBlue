d18d7c440bd85ff51de1a581fbc09d681028697b
Peter Leonard <plen@jive.com>
2015-08-19 11:56:27 -0600

    Removed audio player things we don't use. Added some instructions and put iAds in

--- CarBlue/ViewController.swift
+++ CarBlue/ViewController.swift
@@ -9,27 +9,36 @@
 import UIKit
 import AVFoundation
 import MediaPlayer
+import iAd
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ADBannerViewDelegate {
     
-    @IBOutlet weak var playBTN: UIButton!
     @IBOutlet weak var volView: MPVolumeView!
-    var audioPlayer = AVAudioPlayer()
+    
+    var bannerAdView = ADBannerView(adType: ADAdType.Banner)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        let path = NSBundle.mainBundle().pathForResource("Edm Trumpets", ofType: "mp3")
-//        var music = NSURL(fileURLWithPath: path!)
+        bannerAdView.delegate = self
+        self.canDisplayBannerAds = true
         configureAudioSession()
-//        audioPlayer = AVAudioPlayer(contentsOfURL: music, error: nil)
-//        audioPlayer.prepareToPlay()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func bannerViewDidLoadAd(banner: ADBannerView!) {
+        println("Banner added")
+        self.view.addSubview(banner)
+    }
+    
+    func bannerView(banner: ADBannerView!, didFailToReceiveAdWithError error: NSError!) {
+        println("Failed adding ad")
+        banner.removeFromSuperview()
     }
     
     func configureAudioSession() {
@@ -42,28 +51,12 @@ class ViewController: UIViewController {
             }
         }
         
-        
-        //        if !session.setMode(AVAudioSessionModeVoiceChat, error: &error) {
-        //            println("Could not set mode")
-        //        }
         if !session.setActive(true, error: &error)  {
             println("Could not make session active")
             if let e = error{
                 println(e.localizedDescription)
             }
         }
-        
-        
     }
-    @IBAction func stopTheNoise(sender: AnyObject) {
-//        audioPlayer.stop()
-//        audioPlayer.prepareToPlay()
-    }
-    
-    @IBAction func playBTN(sender: AnyObject) {
-//        audioPlayer.play()
-        println("play")
-    }
-    
 }
 
